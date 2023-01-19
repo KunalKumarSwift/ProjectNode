816a681fd3972eba77a897b9af0c2174355841c6
Kunal Kumar <kunaldhiman91@gmail.com>
2023-01-18 23:01:15 -0500

    adding model

--- controllers/product.js
+++ controllers/product.js
@@ -3,17 +3,19 @@
  !!! This file links models and views.
  */
 
-const products = [];
+const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
   res.render("add-product", { docTitle: "Add product", path: "/add-product" });
 };
 
 exports.postAddProduct = (req, res, next) => {
-  products.push({ title: req.body.title });
+  const product = new Product(req.body.title);
+  product.save();
   res.redirect("/");
 };
 
 exports.getProduct = (req, res, next) => {
+  const products = Product.fetchAll();
   res.render("shop", { prods: products, docTitle: "Shop", path: "/" });
 };
